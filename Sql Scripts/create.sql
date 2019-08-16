c1da21af4239428073764f2b985513d82c012d79
jayesh-kaza <52232556+jayesh-kaza@users.noreply.github.com>
2019-08-16 16:18:09 +0530

    Update create.sql

--- Sql Scripts/create.sql
+++ Sql Scripts/create.sql
@@ -3,9 +3,11 @@ CREATE USER 'springstudent'@'localhost' IDENTIFIED BY 'springstudent';
 GRANT ALL PRIVILEGES ON * . * TO 'springstudent'@'localhost';
 
 
-CREATE DATABASE  IF NOT EXISTS `web_customer_tracker`
+CREATE DATABASE  IF NOT EXISTS `web_customer_tracker`;
 
-CREATE TABLE `customer` (
+use `web_customer_tracker;`
+
+CREATE TABLE `Customer` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(45) DEFAULT NULL,
   `last_name` varchar(45) DEFAULT NULL,
