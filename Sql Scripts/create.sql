a235516360e08424a9a5d5aac06f959092f200a8
Jayesh Kaza <jayesh.kaza@zemosolabs.com>
2019-08-16 15:57:03 +0530

    inital commit

---
+++ Sql Scripts/create.sql
@@ -0,0 +1,17 @@
+CREATE USER 'springstudent'@'localhost' IDENTIFIED BY 'springstudent';
+
+GRANT ALL PRIVILEGES ON * . * TO 'springstudent'@'localhost';
+
+
+CREATE DATABASE  IF NOT EXISTS `web_customer_tracker`
+
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)
+
+
+
